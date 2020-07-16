bdad77f1829475bf60245a896ec459423045ecc3
mkthewlis <mkthewlis@gmail.com>
2020-07-16 14:55:51 +0000

    added template html files to set up for app progress

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from bson.objectid import ObjectId 
 
 #Keeps eny.py private by being listed on gitignore file
 from os import path
